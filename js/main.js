9ce635d291d6ffb154c48291355ef42ea4db6e20
SaraJenkins7 <sarajenkins414@gmail.com>
2021-12-09 18:44:24 -0500

    Updated code

--- js/main.js
+++ js/main.js
@@ -1,41 +1,53 @@
-function createElemWithText(elemType = "p", className) {
+function createElemWithText(elemType = "p", textContent = "", className) {
   const myElem = document.createElement(elemType);
-  myElem.id = "p";
-  myElem.textContent = "";
+  myElem.textContent = textContent;
   if (className) myElem.classList.add(className);
-  return myElem; 
+  return myElem;
 }
 
 function createSelectOptions(jsonData) {
-  if (!jsonData){}
+  if (!jsonData) {
+    return;
+  } else {
+    const user = fetch ("https://jsonplaceholder.typicode.com/users");
+    const option = document.createElement(user);
+    option.value = user.id;
+    option.textContent = user.name;
+    return option;
+  }
 }
 
 function toggleCommentSection(postId) {
-  
+  const button = document.querySelector(`button[data-post-id='${postId}']`);
+  if (!postId) return;
+  //toggleClick = function() {
+       // document.querySelector(button).classList.toggle("hide");
+ // }
 }
 
 function toggleCommentButton(postId) {
+  if (!jsonData) return;
   var clicked = false;
   if(!clicked){
       clicked = true;
-      document.getElementById("btn").textContent = "Show Comments"; 
-  }else{
-      clicked = false;
+      document.getElementById("btn").textContent = "Show Comments";
+  } else {
+      clicked = true;
       document.getElementById("btn").textContent = "Hide Comments";
   }
 }
 
 const deleteChildElements = (parentElement) => {
+  if (!parentElement) return;
   let child = parentElement.lastElementChild;
-  while(child) {
-      parentElement.removeChild(child);
-      child = parentElement.lastElementChild(child);
+  while (parentElement.child) {
+      parentElement.removeChild(parentElement.child);
+      child = parentElement.lastElementChild;
   }
 }
 
 function addButtonListeners() {
-  const button = document.querySelector(`button[data-post-id='${postId}']`);
-  button.addEventListener("click", function (e) {toggleComments(e, postId)}, false);
+   
 }
 
 function removeButtonListeners() {
@@ -57,21 +69,15 @@ const getUsers = async() => {
 }
 
 const getUserPosts = async(userId) => {
-  const userPosts = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-  const jsonUserPosts = await userPosts.json();
-  return jsonUserPosts;
+
 }
 
 const getUser = async(userId) => {
-  const users = await fetch("https://jsonplaceholder.typicode/users");
-  const jsonUsers = await users.json();
-  return jsonUsers;
+
 }
 
 const getPostComments = async(postId) => {
-  const postComments = await fetch("https://jsonplaceholder.typicode/posts/1/comments");
-  const jsonPostComments = await postComments.json();
-  return jsonPostComments;
+
 }
 
 const displayComments = async(postId) => {
@@ -79,16 +85,7 @@ const displayComments = async(postId) => {
 }
 
 const createPosts = async(posts) => {
-  if (!posts === ""){
-      return;
-  } else {
-      const post = await posts;
-      const fragment = document.createDocumentFragment();
-      const article = document.createElement("article");
-    for (let i = 0; i < posts.length; i++) {
-        let post = posts[i];
-    }
-  }
+
 }
 
 const displayPosts = async(posts) => {
@@ -104,11 +101,7 @@ const refreshPosts = async(posts) => {
 }
 
 const selectMenuChangeEventHandler = async(event) => {
-  const selectMenu = document.querySelector("select");
-  selectMenu.disabled = ture;
-
-  selectMenu.disabled = false;
-  return refreshPosts;
+ 
 }
 
 const initPage = async() => {
